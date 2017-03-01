685ce75347499f424674d3199c1bc5306cb2865d
Philip O'Toole <philip.otoole@yahoo.com>
2017-02-28 21:45:59 -0800

    Full use of DynamoDB sets

--- handler.py
+++ handler.py
@@ -28,6 +28,15 @@ def respondMethodNotAllowed(msg):
         },
     }
     
+def respondBadRequest(msg):
+    return {
+        'statusCode': '400',
+        'body': json.dumps({'error': msg}),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+    
 def respondConflict(msg):
     return {
         'statusCode': '409',
@@ -45,6 +54,17 @@ def respondNotFound(msg):
             'Content-Type': 'application/json',
         },
     }
+    
+def serialize_item(i):
+    try:
+        nodes = i['nodes']
+    except KeyError:
+        nodes = []
+    return {
+        'created_at': i['created_at'],
+        TABLE_KEY: i[TABLE_KEY],
+        'nodes': [n for n in nodes]
+    }
 
 def lambda_handler(event, context):
     # Get the DDB table.
@@ -61,13 +81,12 @@ def lambda_handler(event, context):
         id = str(uuid.uuid1())
         i = {
             TABLE_KEY: id,
-            'nodes': [],
             'created_at': str(datetime.datetime.utcnow())
         }
             
         # Store the new ID.
         dynamo.put_item(Item=i)
-        return respondOK(i)
+        return respondOK(serialize_item(i))
 
     # A resource has been supplied -- access it.
     id = resource['proxy']
@@ -77,9 +96,9 @@ def lambda_handler(event, context):
         return respondNotFound('%s does not xx exist' % id)
         
     if operation == 'GET':
-        return respondOK(i)
-    elif operation == 'POST':
-        # Get the address, and add it to the cluster.
+        return respondOK(serialize_item(i))
+    elif operation == 'POST' or operation == 'DELETE':
+        # Get the node address from the request.
         try:
             b = json.loads(event['body'])
         except:
@@ -94,22 +113,19 @@ def lambda_handler(event, context):
         # All good, modify the list of nodes.
         key = {TABLE_KEY: i[TABLE_KEY]}
         
-        try:
-            dynamo.update_item(
-                Key=key,
-                UpdateExpression='SET nodes = list_append(nodes, :i)',
-                ConditionExpression='not contains (nodes, :ix)',
-                ExpressionAttributeValues={':i': [addr], ':ix': addr}
-            )
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
-                # Make reregistration of an address idempotent.
-                pass
-            else:
-                raise e
+        if operation == 'POST':
+            expr = 'add nodes :n'
+        else:
+            expr = 'delete nodes :n'
+            
+        dynamo.update_item(
+            Key=key,
+            UpdateExpression=expr,
+            ExpressionAttributeValues={':n': set([addr])}
+        )
         
         # Return the updated object.
         i = dynamo.get_item(Key={TABLE_KEY: id}, ConsistentRead=True)['Item']
-        return respondOK(i)
+        return respondOK(serialize_item(i))
 
     return respondMethodNotAllowed('unsupported method "{}"'.format(operation))
